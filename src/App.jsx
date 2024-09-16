c6bd2acf3344c9f6e9c69fc30fb748e6087e657c
Sonia Muniz <soninhamusi@gmail.com>
2024-09-16 07:39:56 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+//import { useState } from 'react'
+import './App.css'
+import ColorPicker from './ColoPicker.jsx';
+function App() {
+
+  return(
+    <ColorPicker/>
+  );
+  
+}
+
+export default App
